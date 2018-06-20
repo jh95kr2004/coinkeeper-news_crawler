d49cd8f7a162b52ea8674b36759203cf26afd46b
Junghoon Jang <junghoonjang@Junghoonui-MacBook-Pro.local>
2018-06-20 17:34:19 +0900

    Added azure key file.

--- news.py
+++ news.py
@@ -1,22 +1,29 @@
+#!/usr/bin/python
+
 import requests
 import time
+import json
 
-subscription_key = "1b44b3da8fd5411bbf8e8dc5c0018d47"
+with open("azure.key", "r") as keyFile :
+	data = json.load(keyFile)
+	subscription_key = data["azure_key"]
+	keyFile.close()
+
 search_url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search"
 search_term = "비트코인"
 
 headers = {"Ocp-Apim-Subscription-Key" : subscription_key}
-params  = {"q": search_term,
-           "textDecorations": False,
-           "safeSearch": "Strict",
-           "sortBy": "Date",
-           "since": int(time.time())}
+params  = {
+		"q": search_term,
+		"textDecorations": False,
+	    "safeSearch": "Strict"
+	    # "sortBy": "Date",
+	    # "since": int(time.time())
+	}
 
 response = requests.get(search_url, headers=headers, params=params)
 response.raise_for_status()
 search_results = response.json()
-
-print(search_results["totalEstimatedMatches"])
 
 for article in search_results["value"] :
     print(article["name"])
